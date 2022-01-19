24e5a48eb9c09b9d61d6c71437a7163e27f549db
ohmy04180 <ohmy0418@mobigen.com>
2022-01-19 19:34:21 +0900

    feat: storybook install, 예시 login 페이지 생성

---
+++ source/frontend/src/components/login.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div>여기는 LOGIN FORM 이 들어갑니다.</div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "Login",
+  extends: {},
+  props: {
+    bar: {},
+    foo: {},
+    fooBar: {}
+  },
+  computed: {},
+  components: {},
+  watch: {},
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+// @import ""
+</style>
---
+++ source/frontend/src/stories/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <div>
+        <login/>
+      </div>
+      <div>
+        <my-button size="small" @onClick="onLogout" label="Log out" v-if="user" />
+        <my-button size="small" @onClick="onLogin" label="Log in" v-if="!user" />
+        <my-button primary size="small" @onClick="onCreateAccount" label="Sign up" v-if="!user" />
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import './header.css';
+import MyButton from './Button.vue';
+import Login from '../components/login'
+
+export default {
+  name: 'my-header',
+
+  components: { MyButton, Login },
+
+  props: {
+    user: {
+      type: Object,
+    },
+  },
+
+  methods: {
+    onLogin() {
+      this.$emit('onLogin');
+    },
+    onLogout() {
+      this.$emit('onLogout');
+    },
+    onCreateAccount() {
+      this.$emit('onCreateAccount');
+    },
+  },
+};
+</script>
