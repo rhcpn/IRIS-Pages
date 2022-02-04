c2974a62913c73420fa1ccf40e8d824d16e25d26
ohmy04180 <ohmy0418@mobigen.com>
2022-02-04 11:32:47 +0900

    refactor: 불필요한 Sentry, Integrations 제거

--- source/frontend/src/modules/login/login.js
+++ source/frontend/src/modules/login/login.js
@@ -2,8 +2,6 @@ import Vue from "vue";
 import store from "@/store/login";
 import api from "@/utils/api";
 import i18n from "@/utils/i18n";
-import * as Sentry from "@sentry/vue";
-import { Integrations } from "@sentry/tracing";
 import cookie from "vue-cookies";
 import AxiosInterceptor from "@/components/common/axios-interceptor.vue";
 import Login from "@modules/login/Login.vue";
@@ -21,18 +19,6 @@ Vue.prototype.$api = api;
 
 Vue.component("axios-interceptor", AxiosInterceptor);
 Vue.component("icon", VueSvgIcon);
-
-Sentry.init({
-  Vue,
-  dsn:
-    "https://c10f0211c0bb41af973f1f5ebf7fda60@o924939.ingest.sentry.io/6044951",
-  integrations: [
-    new Integrations.BrowserTracing({
-      tracingOrigins: [/^\//]
-    })
-  ],
-  tracesSampleRate: 1.0
-});
 
 new Vue({
   store,
--- source/frontend/src/modules/main/main.js
+++ source/frontend/src/modules/main/main.js
@@ -4,8 +4,6 @@ import api from "@/utils/api";
 import i18n from "@/utils/i18n";
 import cookie from "vue-cookies";
 import router from "@/router/route";
-import * as Sentry from "@sentry/vue";
-import { Integrations } from "@sentry/tracing";
 import AxiosInterceptor from "@/components/common/axios-interceptor.vue";
 import Main from "@modules/main/Main.vue";
 
@@ -23,18 +21,6 @@ Vue.prototype.$api = api;
 Vue.component("axios-interceptor", AxiosInterceptor);
 Vue.component("icon", VueSvgIcon);
 
-Sentry.init({
-  Vue,
-  dsn:
-    "https://c10f0211c0bb41af973f1f5ebf7fda60@o924939.ingest.sentry.io/6044951",
-  integrations: [
-    new Integrations.BrowserTracing({
-      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-      tracingOrigins: [/^\//]
-    })
-  ],
-  tracesSampleRate: 1.0
-});
 
 new Vue({
   store,
