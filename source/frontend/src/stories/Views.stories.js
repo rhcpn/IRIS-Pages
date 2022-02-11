6cc8267d4d31e2454f4859e9e168f914ca728cb6
ohmy04180 <ohmy0418@mobigen.com>
2022-02-11 17:10:56 +0900

    feat: 스토리북 컴포넌트 유형 추가 (svg 이슈 있음)

--- source/frontend/src/stories/Views.stories.js
+++ source/frontend/src/stories/Views.stories.js
@@ -1,6 +1,8 @@
 import TheHeader from '../components/the-header/the-header.vue';
 import LoginForm from '../modules/login/components/login-form/login-form.vue';
-
+import TheModal from '../components/the-modal/the-modal.vue';
+import ReportScreen from '../modules/main/report-view/components/report-screen/report-screen.vue'
+import ReportSlider from '../modules/main/report-view/components/report-slider/report-slider.vue'
 export default {title: 'Common Component'}
 
 const HeaderTemplate = () => ({
@@ -13,5 +15,23 @@ const LoginTemplate = () => ({
     template: '<login-form />',
 });
 
+const ModalTemplate = () => ({
+    components: { TheModal },
+    template: '<the-modal><template v-slot:header>Modal Header</template><template v-slot:body>MODAL Body</template></the-modal>',
+});
+
+const ReportScreenTemplate = () => ({
+    components: { ReportScreen },
+    template: '<report-screen />',
+});
+
+const ReportSliderTemplate = () => ({
+    components: { ReportSlider },
+    template: '<report-slider />',
+});
+
 export const Header = HeaderTemplate.bind({});
 export const Login = LoginTemplate.bind({});
+export const Modal = ModalTemplate.bind({});
+export const Screen = ReportScreenTemplate.bind({});
+export const Slider = ReportSliderTemplate.bind({});
