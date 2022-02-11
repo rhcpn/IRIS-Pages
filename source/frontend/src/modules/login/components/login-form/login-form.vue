bd948fc50661909a5a17690f0bbb37e81ed114c3
codemotion19 <codemotion@mobigen.com>
2022-02-11 12:36:24 +0900

    refactor: code refactor

--- source/frontend/src/modules/login/components/login-form/login-form.vue
+++ source/frontend/src/modules/login/components/login-form/login-form.vue
@@ -1,46 +1,47 @@
 <template lang="html">
-    <form class="form login-form" @submit.prevent="submit">
-      <h1 class="login-form__title">Login</h1>
-      <div class="login-form__input-box">
-        <div class="login-form__label">
-          <label for="username">User Name</label>
-        </div>
-        <input
-            class="text-input text-input--lg"
-            type="text"
-            id="username"
-            v-model="username"
-            maxlength="10"
-            required
-            placeholder="User Email"
-        />
-        <div class="login-form__label">
-            <label for="password">Password</label>
-        </div>
-        <input
-            class="text-input text-input--lg"
-            type="password"
-            id="password"
-            v-model="password"
-            maxlength="15"
-            required
-            placeholder="Password"
-        />
-        <div class="login-form__option">
-          <div class="checkbox">
-            <input id="saveUser" type="checkbox" value="label" class="checkbox__input login-form-option__save-username-check">
-            <label for="saveUser" class="checkbox__label">
-              <span class="login-form-option__save-username-check-lable-text">Remember me</span>
-            </label>
-          </div>
+  <form class="form login-form" @submit.prevent="submit">
+    <h1 class="login-form__title">Login</h1>
+    <div class="login-form__input-box">
+      <div class="login-form__label">
+        <label for="username">User Name</label>
+      </div>
+      <input
+          class="text-input text-input--lg"
+          type="text"
+          id="username"
+          v-model="username"
+          maxlength="10"
+          required
+          placeholder="User Email"
+      />
+      <div class="login-form__label">
+        <label for="password">Password</label>
+      </div>
+      <input
+          class="text-input text-input--lg"
+          type="password"
+          id="password"
+          v-model="password"
+          maxlength="15"
+          required
+          placeholder="Password"
+      />
+      <div class="login-form__option">
+        <div class="checkbox">
+          <input id="saveUser" type="checkbox" value="label"
+                 class="checkbox__input login-form-option__save-username-check">
+          <label for="saveUser" class="checkbox__label">
+            <span class="login-form-option__save-username-check-lable-text">Remember me</span>
+          </label>
         </div>
       </div>
-      <div class="login-form__button">
-        <button type="submit" class="button button--primary button--lg w-12_12">
-          LOGIN
-        </button>
-      </div>
-    </form>
+    </div>
+    <div class="login-form__button">
+      <button type="submit" class="button button--primary button--lg w-12_12">
+        LOGIN
+      </button>
+    </div>
+  </form>
 </template>
 
 <script type="text/javascript">
