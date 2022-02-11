84e506c225d02abaed2028a1d25a8ffeb98cdb45
ohmy04180 <ohmy0418@mobigen.com>
2022-02-11 15:35:24 +0900

    feat: 미리모기 모달 퍼블리싱 진행

---
+++ source/frontend/src/components/the-modal/the-modal.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+  <transition name="modal" @click="$emit('close-modal')">
+    <div class="modal-overlay">
+      <div class="modal">
+        <div class="modal__head">
+          <slot name="header">
+            <span class="modal__head-close-button">
+            <button class="button button--icon button--link button--sm" @click="$emit('close-modal')">
+              <icon class="svg-icon" data="@icon/close.svg"></icon>
+            </button>
+          </span>
+          </slot>
+        </div>
+        <div class="modal__body">
+          <slot name="body"></slot>
+        </div>
+        <div class="modal__foot">
+          <slot name="footer"></slot>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "TheModal",
+  extends: {},
+  props: {
+    bar: {},
+    foo: {},
+    fooBar: {}
+  },
+  computed: {},
+  components: {},
+  watch: {},
+  methods: {
+  }
+};
+</script>
+
+<style lang="scss">
+@import "src/components/the-modal/the-modal";
+</style>
--- source/frontend/src/modules/main/report-list/report-list.vue
+++ source/frontend/src/modules/main/report-list/report-list.vue
@@ -1,23 +1,44 @@
 <template lang="html">
   <div id="list">
-  <the-header></the-header>
-  <router-link to="/app/views">보고서 보기 화면으로 이동 </router-link>
+    <the-header></the-header>
+    <button type="button" @click="openModal">모달 열기</button>
+    <the-modal v-show="isOpenModal" @close-modal="closeModal">
+      <template v-slot:body>
+        <img src="../../../assets/style-product/images/screen-sample2.png" alt="미리보기">
+      </template>
+    </the-modal>
+    <router-link to="/app/views">보고서 보기 화면으로 이동</router-link>
   </div>
 </template>
 
 <script type="text/javascript">
 import TheHeader from "@/components/the-header/the-header";
+import TheModal from "@/components/the-modal/the-modal"
 
 export default {
   name: "ReportList",
   extends: {},
+  data(){
+    return{
+    isOpenModal: false
+    }
+  },
   props: {},
-  computed: {},
+  computed: {
+  },
   components: {
-    TheHeader
+    TheHeader,
+    TheModal
   },
   watch: {},
-  methods: {}
+  methods: {
+    openModal(){
+      this.isOpenModal = true;
+    },
+    closeModal(){
+      this.isOpenModal = false;
+    }
+  }
 };
 </script>
 
