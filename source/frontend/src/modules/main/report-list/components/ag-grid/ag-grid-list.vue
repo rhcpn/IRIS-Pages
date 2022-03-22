65c1e1a06fef703ca3ff6dba712b3757b4d71717
codemotion19 <codemotion@mobigen.com>
2022-03-22 11:35:21 +0900

    refactor: 코드리뷰 반영

--- source/frontend/src/modules/main/report-list/components/ag-grid/ag-grid-list.vue
+++ source/frontend/src/modules/main/report-list/components/ag-grid/ag-grid-list.vue
@@ -3,7 +3,7 @@
     <div class="list-layout__top-bar">
       <div class="list-layout__filter">
         <div class="checkbox">
-          <input type="chckbox" class="checkbox__input" />
+          <input type="checkbox" class="checkbox__input" />
           <label for="" class="checkbox__label"></label>
         </div>
         <strong class="list-layout__filter-title">
@@ -15,37 +15,37 @@
       <div class="list-layout__search-bar">
         <div class="text-input-group text-input-group--lg">
           <input
-              type="text"
-              class="text-input text-input--lg"
-              placeholder="검색어를 입력해주세요."
-              ref="search"
-              @keyup="onQuickFilterChanged"
+            type="text"
+            class="text-input text-input--lg"
+            placeholder="검색어를 입력해주세요."
+            ref="search"
+            @keyup="onQuickFilterChanged"
           />
           <button class="text-input-group__clear-button" @click="resetInput">
             <icon
-                class="svg-icon svg-fill"
-                data="@/assets/style-core/images/icon/close.svg"
+              class="svg-icon svg-fill"
+              data="@/assets/style-core/images/icon/close.svg"
             ></icon>
           </button>
           <button
-              title="검색"
-              class="text-input-group__search-button button button--icon button--tertiary button--lg"
+            title="검색"
+            class="text-input-group__search-button button button--icon button--tertiary button--lg"
           >
             <icon
-                class="svg-icon svg-fill"
-                data="@/assets/style-core/images/icon/search.svg"
+              class="svg-icon svg-fill"
+              data="@/assets/style-core/images/icon/search.svg"
             ></icon>
           </button>
         </div>
         <div class="list-layout__search-bar-button">
           <router-link to="/app/views">
             <button
-                @click="getSelectedRows()"
-                class="button button--lg button--primary"
+              @click="getSelectedRows()"
+              class="button button--lg button--primary"
             >
               <icon
-                  class="svg-icon svg-fill"
-                  data="@/assets/style-core/images/icon/eye.svg"
+                class="svg-icon svg-fill"
+                data="@/assets/style-core/images/icon/eye.svg"
               ></icon>
               Show
             </button>
@@ -54,11 +54,11 @@
       </div>
     </div>
     <ag-grid-vue
-        class="ag-theme-balham"
-        :columnDefs="columnDefs"
-        :rowData="rowData"
-        :gridOptions="gridOptions"
-        rowSelection="multiple"
+      class="ag-theme-balham"
+      :columnDefs="columnDefs"
+      :rowData="rowData"
+      rowSelection="multiple"
+      @cell-clicked="onCellClicked"
     >
     </ag-grid-vue>
   </div>
@@ -69,14 +69,7 @@ import { AgGridVue } from "/node_modules/ag-grid-vue/lib/AgGridVue.js";
 
 export default {
   name: "AgGridList",
-  data() {
-    return {
-      columnDefs: null,
-      rowData: [],
-      columnApi: null,
-      girdOptions: null,
-    };
-  },
+  props: ["columnDefs", "rowData"],
   components: {
     AgGridVue
   },
@@ -84,48 +77,13 @@ export default {
     resetInput() {
       this.$refs["search"].value = "";
     },
-    onQuickFilterChanged (event) {
-      this.gridOptions.api.setQuickFilter(event.target.value)
+    onQuickFilterChanged(event) {
+      this.gridOptions.api.setQuickFilter(event.target.value);
+    },
+    onCellClicked(event) {
+      console.log("onCellClicked");
     }
-  },
-  beforeMount() {
-    this.columnDefs = [
-      {
-        filed: "",
-        checkboxSelection: true,
-        headerCheckboxSelection: true,
-        width: 80,
-        sortable: false,
-        cellStyle: { justifyContent: "center" }
-      },
-      {
-        field: "categoryName",
-        sortable: true,
-        filter: false,
-        width: 200,
-        cellStyle: { justifyContent: "center" }
-      },
-      { field: "name", sortable: true, filter: false, width: 1100 },
-      {
-        field: "userId",
-        sortable: true,
-        filter: false,
-        width: 260,
-        cellStyle: { justifyContent: "center" }
-      },
-      {
-        field: "editDate",
-        sortable: true,
-        filter: false,
-        width: 230,
-        cellStyle: { justifyContent: "center" }
-      }
-    ];
-
-    fetch("http://localhost:3000/list")
-        .then((result) => result.json())
-        .then((rowData) => (this.rowData = rowData));
-  },
+  }
 };
 </script>
 
--- source/frontend/src/modules/main/report-list/report-list.vue
+++ source/frontend/src/modules/main/report-list/report-list.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <div id="list">
-    <ag-grid-list></ag-grid-list>
+    <ag-grid-list :column-defs="columnDefs" :row-data="rowData"></ag-grid-list>
   </div>
 </template>
 
@@ -11,14 +11,57 @@ export default {
   name: "ReportList",
   extends: {},
   props: {},
-  computed: {},
   components: {
     AgGridList
   },
+  data() {
+    return {
+      rowData: [],
+      gridOptions: null
+    };
+  },
+  beforeMount() {
+    this.columnDefs = [
+      {
+        filed: "",
+        checkboxSelection: true,
+        headerCheckboxSelection: true,
+        width: 80,
+        sortable: false,
+        cellStyle: { justifyContent: "center" }
+      },
+      {
+        field: "categoryName",
+        sortable: true,
+        filter: false,
+        width: 200,
+        cellStyle: { justifyContent: "center" }
+      },
+      { field: "name", sortable: true, filter: false, width: 1100 },
+      {
+        field: "userId",
+        sortable: true,
+        filter: false,
+        width: 260,
+        cellStyle: { justifyContent: "center" }
+      },
+      {
+        field: "editDate",
+        sortable: true,
+        filter: false,
+        width: 230,
+        cellStyle: { justifyContent: "center" }
+      }
+    ];
+
+    fetch("http://localhost:3000/list")
+      .then((result) => result.json())
+      .then((rowData) => (this.rowData = rowData));
+  },
+  computed: {},
   watch: {},
   methods: {}
 };
 </script>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
